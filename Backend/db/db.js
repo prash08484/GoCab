212bfea840214c304fd06ef9719176ab88501fbf
Prashant Yadav <prash08484@gmail.com>
2025-03-20 00:18:19 +0530

    db init

---
+++ Backend/db/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+function connectToDB() {
+    mongoose.connect(process.env.DB_CONNECT)
+        .then(() => {
+            console.log("✅ Connected to DB");
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+}
+
+module.exports = connectToDB;
--- Backend/server.js
+++ Backend/server.js
@@ -4,7 +4,6 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
 
-
 server.listen(port,() => {
     console.log(`✅ Server listening on Port ${port}`);
 });
