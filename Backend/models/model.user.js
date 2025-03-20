53f07296864236dc26e235686a2dd2030e9fb3f0
Prashant Yadav <prash08484@gmail.com>
2025-03-21 01:14:25 +0530

    adding expires time in jwt

--- Backend/models/model.user.js
+++ Backend/models/model.user.js
@@ -32,7 +32,7 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.methods.generateAuthTokens = function () {
-    const token = jwt.sign({ _id: this.id }, process.env.JWT_SECRET);
+    const token = jwt.sign({ _id: this.id }, process.env.JWT_SECRET,{expiresIn:'24h'});
     return token; }
 
 userSchema.methods.comparePassword = async function (password) {
